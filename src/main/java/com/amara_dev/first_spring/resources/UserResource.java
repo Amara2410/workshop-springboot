356783b6e6c564a0e8a660f18f4333d9cafbceb3
Amara Santos <yarasousasantos24@gmail.com>
2024-11-26 14:17:49 -0300

    User entity and resource

---
+++ src/main/java/com/amara_dev/first_spring/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.amara_dev.first_spring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amara_dev.first_spring.entities.User;
+
+@RestController
+@RequestMapping(value ="/user")
+public class UserResource { 
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User (1L, "Yara Amara Sousa Santos", "yara@gmail.com", "999999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}

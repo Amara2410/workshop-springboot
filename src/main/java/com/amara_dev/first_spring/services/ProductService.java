ca9ef84610805047e5e2ca0f4c897239aa15f407
Amara Santos <yarasousasantos24@gmail.com>
2024-11-28 18:28:25 -0300

    Product

---
+++ src/main/java/com/amara_dev/first_spring/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.amara_dev.first_spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.amara_dev.first_spring.entities.Product;
+import com.amara_dev.first_spring.entities.User;
+import com.amara_dev.first_spring.repositories.ProductRepository;
+
+@Component
+public class ProductService {
+	 
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();	
+	}
+	
+	public Product findById(Long id) {
+	Optional<Product> obj = repository.findById(id);
+	return obj.get();
+	}
+
+}
